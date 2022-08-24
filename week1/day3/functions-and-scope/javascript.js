c5f1d454b7feebb1b9175c49f84cdaa0f21bfd63
guillemreig <guillemreiggrau@gmail.com>
2022-08-24 18:27:14 +0200

    Found a better way to do the javascript exercise

--- week1/day3/functions-and-scope/javascript.js
+++ week1/day3/functions-and-scope/javascript.js
@@ -12,13 +12,14 @@ console.log(sumAll(5, 10, 15, 100, 200)); //We test the function with the exampl
 // Part 2
 function waitThenRun(func) {
     setTimeout(func, 1500);
-} //Inside the waitThenRun function we run the built-in setTimeout function, and we reuse in it the argument.
+} //Inside the waitThenRun function we run the built-in setTimeout function, and we pass it the argument-function.
 
-function testFunction() {
-    console.log("This should appear last!");
-} //The function that we will provide as an argument for testing purposes.
-
-waitThenRun(testFunction);
+waitThenRun(function () {
+    console.log("Hello!");
+    waitThenRun(function () {
+        console.log("Goodbye!");
+    }); // logs 'Goodbye!' 1.5 seconds later
+}); // logs 'Hello!' 1.5 seconds later
 
 // Part 3
 // num <= 0 || isNaN(num) === true
