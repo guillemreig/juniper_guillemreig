7215fb45eda5914ebab43dbf7a8ea69dd8677efe
guillemreig <guillemreiggrau@gmail.com>
2022-08-26 13:52:11 +0200

    Code after assigning numbers.y the value of doubleX

--- dictation.js
+++ dictation.js
@@ -1,2 +1,25 @@
 var x;
 var doubleX;
+
+x = 7;
+
+function timesTwo(num) {
+    return num * 2;
+}
+
+doubleX = timesTwo(x);
+
+var numbers;
+
+numbers = [x, doubleX];
+
+for (var element of numbers) {
+    console.log(element);
+}
+
+numbers = {};
+
+numbers.y = doubleX;
+
+// For testing purposes
+console.log(numbers);
