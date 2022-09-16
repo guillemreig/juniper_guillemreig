d6fa1bc96dda090152af22e935e21047f3962747
guillemreig <guillemreiggrau@gmail.com>
2022-09-16 19:37:22 +0200

    Updated exercise 1 to use querystring

--- week4/day20_node/index.js
+++ week4/day20_node/index.js
@@ -1,25 +1,23 @@
 // Part 1
 
 const url = require("url");
+const querystring = require("querystring");
 
 const urlArr = process.argv.slice(2); // Ignores the first two elements of the 'argv' array (we don't need them)
-// console.log("urlArr", urlArr);
 
 for (let iUrl of urlArr) {
     const { protocol, host, hostname, port, pathname, query } = url.parse(iUrl); // Assign property values to a group of variables
     const urlObj = { protocol, host, hostname, port, pathname, query }; // Create a new object with variables as properties and their values. This method allow us to change property order
-    // const { slashes, auth, hash, search, path, href, ...urlObj } = url.parse(iUrl); // Creates a shallow copy skipping the properties that we won't use. This
-    //console.log("urlObj :", urlObj);
+    // const { slashes, auth, hash, search, path, href, ...urlObj } = url.parse(iUrl); // Creates a shallow copy skipping the properties that we won't use.
 
     for (let property in urlObj) {
         console.log(`The ${property} is ${urlObj[property]}`); // Iterates through each property and logs template literal
     }
 
     if (urlObj.query) {
-        //console.log("urlObj.query.split('&') :", urlObj.query.split("&"));
-        const queryArr = urlObj.query.split("&"); // Makes an array with each query parameter
-        for (let para of queryArr) {
-            console.log(`The value of the ${para.split("=")[0]} parameter is ${para.split("=")[1]}`); // Iterates through each parameter and logs a template literal
+        const queryObj = querystring.parse(urlObj.query); // 'querystring' converts the query into an object: { a: '100', b: '200,' }
+        for (let property in queryObj) {
+            console.log(`The value of the ${property} parameter is ${queryObj[property]}`); // Iterates through each property and logs template literal
         }
     } // If query is not 'null', use another template literal to log its contents
     console.log(); // Creates an empty line for easier reading
