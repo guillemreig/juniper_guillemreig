6dfeffa01cab8c6c8cccfd3977b343632f01cdba
guillemreig <guillemreiggrau@gmail.com>
2022-09-18 19:43:43 +0200

    Game works but code is convoluted and tedious to expand. Will remake base code.

--- week4/day20_node/juniper-textadventure/script2.js
+++ week4/day20_node/juniper-textadventure/script2.js
@@ -7,23 +7,7 @@ const rl = readline.createInterface({
 });
 ///// do not touch /////
 
-/*
-rl.question("Do you enjoy learning Node.js?", function (answer) {
-    if (answer === "yes") {
-        console.log("great!");
-    }
-    rl.close();
-});
-*/
-
-// Choose text adventure color by passing an argument
-
-const chalk = require("chalk");
-
-let color;
-let colorSet = true;
-
-const history = [];
+// Color choice
 
 setColor(process.argv[2]);
 
@@ -54,55 +38,22 @@ function setColor(colorChoice) {
     return;
 }
 
-// Player stats
-const player = {
-    name: "Will",
-    level: 1,
-    hp: 100,
-    mp: 10,
-    combat: 1,
-    magic: 1,
-    charisma: 1,
-    weapon: "none",
-    armor: "none",
-};
-
-const sword = {
-    damage: 5,
-    type: "melee",
-};
-
-const items = {
-    map: "A map of the area",
-};
-
 // Story
-let lastInput;
 
-let intro = {
+let intro0 = {
     func: function () {},
     s: `\nWelcome to the Land of Heinlein! \n
-This is a story of a young adventurer in a land where mythology is more history than myth. This is your story.`,
+    This is a story of a young adventurer in a land where mythology is more history than myth. This is your story.`,
     q: "Would you like to play?",
     answers: {
-        yes: {
-            func: function () {
-                inputStage = true;
-            },
-            q: "What is your name?",
-            answers: {
-                func: function () {
-                    player.name = lastInput;
-                    console.log(player);
-                    next(chapter1);
-                },
-            },
+        yes: function () {
+            next(intro1);
         },
         no: "Alright then. Enjoy your day!",
     },
 };
 
-let chapter1 = {
+let intro1 = {
     func: function () {
         if (brooch) {
             console.log(`Just as you turn to go back to the fork \
@@ -125,44 +76,22 @@ ${chalk.green("map")}, and you couldn't see any sign in the dark.
 ${chalk.yellow("[Tip: You can check your inventory by typing 'items' at any moment.]")}`,
     q: "You are alone in a dark forest and facing a fork in the road. Which direction do you turn?",
     answers: {
-        left: {
-            func: function () {},
-            s: `\nThere's a scary wizard! He asks you a tough question.`,
-            q: "What's 1+1?",
-            answers: {
-                2: "congratulations!",
-            },
-        },
-        right: {
-            func: function () {
-                delete previousStage.answers.right;
-                brooch = true;
-            },
-            s: `\nAfter walking for a while the path turns into a trail.`,
-            q: "This was not the way.",
-            answers: {
-                "go back": "",
-            },
-        },
+        left: function () {},
+        right: function () {},
     },
 };
 
-let stage = intro;
-let previousStage;
-let inputStage = false;
+const player = {
+    name: "will",
+};
 
-// Story vars
+const items = {
+    map: "A map of the region",
+};
+
+next(intro0);
+
 let brooch;
-
-if (!colorSet) {
-    rl.question(`Would you like to use a color the information text?:\n[No,${chalk.red("red")},${chalk.yellow("yellow")},${chalk.green("green")},${chalk.cyan("cyan")},${chalk.blue("blue")},${chalk.magenta("magenta")}]\n`, function (userInput) {
-        setColor(userInput);
-        next(intro);
-        return;
-    });
-} else {
-    next(intro);
-}
 
 // Game function
 function next(stage) {
@@ -174,13 +103,6 @@ function next(stage) {
     delete stage.s;
 
     rl.question(color(stage.q) + ` [${Object.keys(stage.answers)}] \n`, function (userInput) {
-        // Determine if input is valid
-        if (inputStage) {
-            inputStage = false;
-            lastInput = userInput;
-            stage.answers.func();
-            return;
-        }
         // Check items
         if (userInput == "items") {
             console.log(items);
@@ -188,6 +110,7 @@ function next(stage) {
             return;
         }
 
+        // Determine if input is valid
         if (!stage.answers.hasOwnProperty(userInput)) {
             next(stage);
             return;
